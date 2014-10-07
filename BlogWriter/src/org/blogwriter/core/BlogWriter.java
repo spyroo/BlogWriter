a74df14a629722a22071f78e4e771eab6b003a17
djohnson029 <declan.l.johnson@gmail.com>
2014-10-07 11:53:02 -0600

    Need to do window

---
+++ BlogWriter/src/org/blogwriter/core/BlogWriter.java
@@ -0,0 +1,17 @@
+package org.blogwriter.core;
+
+import javax.swing.JFrame;
+
+public class BlogWriter extends JFrame{
+
+	private static final long serialVersionUID = -5153080826068747470L;
+
+	public static void main(String[] args) {
+		new BlogWriter();
+	}
+	
+	public BlogWriter(){
+		
+	}
+
+}
