a875391f712cb6e4a651c9fb957f6a064890e5c5
djohnson029 <declan.l.johnson@gmail.com>
2014-10-09 12:23:35 -0600

    Markdown can now be parsed

--- BlogWriter/src/org/blogwriter/core/BlogWriter.java
+++ BlogWriter/src/org/blogwriter/core/BlogWriter.java
@@ -2,6 +2,8 @@ package org.blogwriter.core;
 
 import javax.swing.JFrame;
 
+import com.github.rjeschke.txtmark.Processor;
+
 public class BlogWriter extends JFrame{
 
 	private static final long serialVersionUID = -5153080826068747470L;
@@ -11,7 +13,8 @@ public class BlogWriter extends JFrame{
 	}
 	
 	public BlogWriter(){
-		
+		String before = Processor.process("This is an [example link](http://example.com/).");
+		System.out.println(before);
 	}
 
 }
